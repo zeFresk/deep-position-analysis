5d5d6779879154f259dd32e39795d3bcc0c49436
zeFresk <8807862+zeFresk@users.noreply.github.com>
2018-12-02 14:32:36 +0100

    Output is prettier,  global progression added, can now use time or nodes to stop search.

--- chess-treepos/chess_treepos.py
+++ chess-treepos/chess_treepos.py
@@ -9,9 +9,25 @@ import chess.uci
 import sys
 import time
 import os.path
+import io
+
+time_st = 0
+
+def format_nodes(n):
+    """Select appropriate form for big values."""
+    if n < 10**3: #less than 3 digits
+        return "%d"%(n)
+    elif n < 10**6: #less than 6 digits => Knodes
+        return "{:1.1f}K".format(n/1000)
+    elif n < 10**9: #less than 9 digits => Mnodes
+        return "{:1.1f}M".format(n/10**6)
+    elif n < 10**12: #less than 9 digits => Gnodes
+        return "{:1.1f}G".format(n/10**9)
+    else: #more than 10^12
+        return "{:1.1f}T".format(n/10**12)
 
 
-def explore_rec(board, engine, pv, depth, nodes):
+def explore_rec(board, engine, pv, depth, nodes, msec = None, tot = None):
     """
         Explore the current pgn position 'depth' plys deep using engine
 
@@ -20,13 +36,21 @@ def explore_rec(board, engine, pv, depth, nodes):
         pv : we will explore top-'pv' moves
         depth : depth of final tree
         nodes : integer representing max nodes to explore per move
+        msec : time in milliseconds before stopping exploration
 
         Returns tree of moves and associated eval
            
         Warning : total nodes computed ~ (pv**depth) * nodes !! Exponential growth !!
     """
+    global pos_index
+    if tot == None: #first turn
+        tot = (pv**depth)-1
+        pos_index = 0
+
     if depth == 0:
         return None
+
+    pos_index += 1
 
     # Creating handler for multi-PV
     info_handler = chess.uci.InfoHandler()
@@ -37,19 +61,39 @@ def explore_rec(board, engine, pv, depth, nodes):
     engine.position(current_board)
 
     # Starting search
-    print(">>> Analysing variation : [%s]..."%(current_board.fen()))
-    cmd = engine.go(nodes=nodes, async_callback=True)
-    sys.stdout.write(">>> 0% : ##\r")
+    elapsed_time = time.clock() - time_st
+
+    if elapsed_time == 0: #avoid divising by 0
+        elapsed_time = 1
+
+    sys.stdout.buffer.close = lambda: None # atrocity but needed
+    out = io.TextIOWrapper(sys.stdout.buffer, line_buffering = False)
+
+    pos_per_s = pos_index/elapsed_time # average positions per second
+    remaining_time_s = (tot-(pos_index-1)) / pos_per_s
+    print(">Analysing variation %d of %d, estimated time remaining : %dh %dm..."%(pos_index, tot, remaining_time_s // (60*60), (remaining_time_s // 60) %60), flush=True)
+    cmd = engine.go(nodes=nodes, movetime=msec, async_callback=True)
+    out.write(">> 0% : ###")
 
     while not cmd.done(): #until search is finished
         time.sleep(0.100)
         with info_handler:
-            if "nodes" in info_handler.info and "pv" in info_handler.info:
-                sys.stdout.write(">>> %d%% : %s\r"%(int((info_handler.info["nodes"]/nodes) * 100),  chess.Board.san(current_board, info_handler.info["pv"][1][0])))
+            if "nodes" in info_handler.info and "pv" in info_handler.info and "nps" in info_handler.info:
+                prct = 0
+                if nodes != None: #we use nodes as stop
+                    prct = int(info_handler.info["nodes"])/nodes
+                else: # we use time as stop
+                    prct = int(info_handler.info["time"])/msec
+
+                out.write("\r" + " "*30) # cleaning line
+                out.write("\r>> {:.1%} @ {:s}nodes/s : {:s}".format(prct, format_nodes(int(info_handler.info["nps"])), chess.Board.san(current_board, info_handler.info["pv"][1][0])))
+                out.flush()
 
 
     # finshed
-    sys.stdout.write("\r>>> 100%% : %s\n"%(chess.Board.san(current_board, info_handler.info["pv"][1][0])))
+    out.write("\r" + " "*30) # cleaning line
+    out.write("\r>> 100%% @ {:s}nodes/s : {:s}\n\n".format(format_nodes(int(info_handler.info["nps"])), chess.Board.san(current_board, info_handler.info["pv"][1][0])))
+    out.flush()
 
     # get all moves in an array
     moves = []
@@ -63,7 +107,7 @@ def explore_rec(board, engine, pv, depth, nodes):
     for (mo, cp) in moves: # explore new moves
         new_board = chess.Board(current_board.fen())
         new_board.push(mo)
-        ret += [[(mo, cp), explore_rec(new_board, engine, pv, depth-1, nodes)]]
+        ret += [[(mo, cp), explore_rec(new_board, engine, pv, depth-1, nodes, msec, tot)]]
     
     return ret
 
@@ -150,7 +194,8 @@ def main():
     parser.add_argument("-c", "--config", dest="engine_config", action="store", type=str, default="<autodiscover>", help="path to engine configuration")
     parser.add_argument("--pv", dest="pv", action="store", type=int, default=2, help="number of best moves to explore per node")
     parser.add_argument("--depth", dest="depth", action="store", type=int, default=2, help="number of plies to explore")
-    parser.add_argument("--nodes", dest="nodes", action="store", type=int, default=50000, help="node used to explore each node in seconds")
+    parser.add_argument("--nodes", dest="nodes", action="store", type=int, default=-1, help="nodes to explore at each step before returning best move")
+    parser.add_argument("--time", dest="sec", action="store", type=int, default=-1, help="time in seconds passed at each step before returning best move")
 
     args = parser.parse_args()
 
@@ -161,6 +206,13 @@ def main():
         sys.stderr.write("!!Error: %s engine doesn't exists ! Exiting...\n"%(args.engine_path))
         sys.exit(-1)
 
+    if args.nodes < 0 and args.sec < 0: #no stopping condition
+        sys.stderr.write("!!Error: No stopping conditions, please set --nodes or --time ! Exiting...\n"%(args.engine_path))
+        sys.exit(-1)
+
+    if args.nodes < 0: args.nodes = None
+    if args.sec < 0: args.sec = None
+
     engine_path = args.engine_path
 
     engine = chess.uci.popen_engine(engine_path)
@@ -168,12 +220,6 @@ def main():
    
     opt = load_options(engine, args.engine_config)
 
-    #opt["Threads"] = 3
-    #opt["NNCacheSize"] = 200000 * 10 # default *10
-    #opt["SyzygyPath"] = "E:/Programs/Arena/TB/syzygy"
-    ##opt["RamLimitMb"] = 2**32
-    #opt["WeightsFile"] = "E:/Programs/Arena/Engines/Lc0-v19/weights_11250.gz"
-    #opt["CacheHistoryLength"] = 2
     opt["MultiPV"] = args.pv
 
     engine.setoption(opt)
@@ -182,26 +228,37 @@ def main():
 
     for filename in args.fen_files:
         file = open(filename)
+        file = file.readlines()
         for (i, position_str) in enumerate(file): #iterate through lines
-            print("Exploring [%s]..."%(position_str))
+            print("\nExploring fen %d of %d : [%s]...\n"%(i+1, len(file), position_str.strip()))
 
             board = chess.Board(position_str) # We load board
 
+            time_st = time.time()
+
             # We generate variations tree
-            tree = explore_rec(board, engine, args.pv, args.depth, args.nodes)
+            msec = None
+            if args.sec != None: msec = args.sec*1000
+            tree = explore_rec(board, engine, args.pv, args.depth, args.nodes, msec)
 
             # We create pgn to export
             pos_pgn = chess.pgn.Game()
-            pos_pgn.headers["Event"] = "DeA at %d nodes, %d ply-depth, of %s"%(args.nodes, args.depth, position_str)
+            stopping = args.nodes if (args.nodes != None) else args.sec
+            pos_pgn.headers["Event"] = "DeA using %s at %s per move, %d ply-depth, of %s"%(engine.name, stopping, args.depth, position_str)
             pos_pgn.setup(board)
 
             # We append the tree
             append_variations(tree, pos_pgn, args.depth)
 
+            elapsed = time.time() - time_st # in seconds
+            print("Completed position analysis %d of %d from %s in %d hours %d minutes %d seconds.\nSaving result.\n"%(i+1, len(file), filename, elapsed // (60*60), (elapsed // 60)%60, elapsed % 60))
+
             # We save the pgn
             index = filename.find(".")
             if index == -1:
                 index = len(filename)
-            print(pos_pgn, file=open("%s%d_%dn%dd.pgn"%(filename[0:index], i, args.nodes, args.depth), "w"), end="\n\n")
+
+            stopping_fmt = (str(args.nodes) +"n") if (args.nodes != None) else (str(args.sec)+"s")
+            print(pos_pgn, file=open("%s%d_%s%dd.pgn"%(filename[0:index], i, stopping_fmt, args.depth), "w"), end="\n\n")
             
 main()
