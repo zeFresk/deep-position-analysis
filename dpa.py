de34156f55bd3f1da3a9b56e21218f757b3d6ab7
zeFresk <8807862+zeFresk@users.noreply.github.com>
2018-12-09 12:30:17 +0100

    Refactored args parsing. Made overall code more flexible.

--- dpa.py
+++ dpa.py
@@ -16,6 +16,58 @@ import traceback
 
 import re
 
+###########################################
+############ Arguments parsing ############
+###########################################
+
+def make_parser():
+    """Create the default parser and setup args configuration."""
+
+    parser = argparse.ArgumentParser(description="Generate pgn of possible variations from position.")
+
+    parser.add_argument("fen_files", metavar='F', type=str, nargs='+', help="fen file to generate variation from")
+    parser.add_argument("-p", "--engine", dest="engine_path", action="store", type=str, required=True, help="path to engine")
+    parser.add_argument("-c", "--config", dest="engine_config", action="store", type=str, default="<autodiscover>", help="path to engine configuration")
+    parser.add_argument("--pv", dest="pv", action="store", type=int, default=2, help="number of best moves to explore per node")
+    parser.add_argument("--depth", dest="depth", action="store", type=int, default=2, help="number of plies to explore")
+    parser.add_argument("--nodes", dest="nodes", action="store", type=int, default=-1, help="nodes to explore at each step before returning best move")
+    parser.add_argument("--time", dest="sec", action="store", type=int, default=-1, help="time in seconds passed at each step before returning best move")
+    parser.add_argument("--tree", dest="tree_exp", action="store_const", const=True, default=False, help="export final tree directly")
+    parser.add_argument("--appending", dest="appending", action="store_const", const=True, default=False, help="append possible continuation to end nodes.") # carefull, inverted
+    
+    return parser
+
+def check_args(args):
+    """Make sure all needed arguments are set correctly, else exit."""
+    if not os.path.isfile(args.engine_path):
+        sys.stderr.write("!!Error: %s engine doesn't exists ! Exiting...\n"%(args.engine_path))
+        sys.exit(-1)
+
+    if args.nodes < 0 and args.sec < 0: #no stopping condition
+        sys.stderr.write("!!Error: No stopping conditions, please set --nodes or --time ! Exiting...\n")
+        sys.exit(-1)
+
+    if args.nodes < 0: args.nodes = None
+    if args.sec < 0: args.sec = None
+
+    return args
+
+def get_args():
+    """Parse all args given to this script. Exit on error."""
+    parser = make_parser()
+
+    args = parser.parse_args()
+    args = check_args(args)
+
+    return args
+
+
+
+
+###########################################
+############ Helper functions #############
+###########################################
+
 def format_nodes(n, fmt="{:1.1f}"):
     """Select appropriate form for big values."""
     if n < 10**3: #less than 3 digits
@@ -45,6 +97,10 @@ def elapsed_since(timestamp):
     It will always returns at least one."""
     ret = time.perf_counter() - timestamp
     return ret if ret >= 1 else 1
+
+###########################################
+####### Core functions & exploration ######
+###########################################
 
 class Explorator(object):
     def __init__(self):
@@ -214,6 +270,10 @@ class Explorator(object):
             return "{:+.2f}".format(self.info_handler.info["score"][1].cp/100.)
 
 
+###########################################
+########## UCI config functions ###########
+###########################################
+
 def write_config(opt, file):
     """Export options dictionnary to config file."""
     if "multiPV" in opt:
@@ -267,6 +327,11 @@ def load_options(engine, config):
         sys.stderr.write("!!Error: config %s doesn't exists ! Exiting...\n")
         sys.exit(-2)
 
+
+###########################################
+######### Tree parsing functions ##########
+###########################################
+
 def append_variations_rec(tree, pgn, depth):
     if depth < 0:
         return
@@ -297,35 +362,16 @@ def append_variations(tree, pgn, depth):
         append_variations_rec(tree[i], pgn, depth - 1)
 
 
-def main():
-    print("Parsing args...")
-    # args parsing
-    parser = argparse.ArgumentParser(description="Generate pgn of possible variations from position.")
-    parser.add_argument("fen_files", metavar='F', type=str, nargs='+', help="fen file to generate variation from")
-    parser.add_argument("-p", "--engine", dest="engine_path", action="store", type=str, required=True, help="path to engine")
-    parser.add_argument("-c", "--config", dest="engine_config", action="store", type=str, default="<autodiscover>", help="path to engine configuration")
-    parser.add_argument("--pv", dest="pv", action="store", type=int, default=2, help="number of best moves to explore per node")
-    parser.add_argument("--depth", dest="depth", action="store", type=int, default=2, help="number of plies to explore")
-    parser.add_argument("--nodes", dest="nodes", action="store", type=int, default=-1, help="nodes to explore at each step before returning best move")
-    parser.add_argument("--time", dest="sec", action="store", type=int, default=-1, help="time in seconds passed at each step before returning best move")
-    parser.add_argument("--tree", dest="tree_exp", action="store_const", const=True, default=False, help="export final tree directly")
-    parser.add_argument("--appending", dest="appending", action="store_const", const=True, default=False, help="append possible continuation to end nodes.") # carefull, inverted
+###########################################
+############### Entry point ###############
+###########################################
 
-    args = parser.parse_args()
+def main():
+    # args parsing
+    args = get_args()
 
     #engine setup
     print("Setting-up engine")
-
-    if not os.path.isfile(args.engine_path):
-        sys.stderr.write("!!Error: %s engine doesn't exists ! Exiting...\n"%(args.engine_path))
-        sys.exit(-1)
-
-    if args.nodes < 0 and args.sec < 0: #no stopping condition
-        sys.stderr.write("!!Error: No stopping conditions, please set --nodes or --time ! Exiting...\n")
-        sys.exit(-1)
-
-    if args.nodes < 0: args.nodes = None
-    if args.sec < 0: args.sec = None
 
     engine_path = args.engine_path
 
